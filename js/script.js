ceee57142c7ae0435b389eff4b56b925f383391d
L.Kit <link2868@ukr.net>
2023-02-06 21:15:47 +0200

    Кнопка мiж кнопкою, при клiку круг справа змiнює колiр. Onclick в HTML, а функцiя в script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 elem.onclick = function() {
     alert ('Привіт учителю!');
 };
+
+let i=1;
+function changeColor(){
+    i++;
+    if (i==2) { document.getElementsByClassName('button-null')[0].style.backgroundColor = '#6d9bf5';};
+    if (i==3) { document.getElementsByClassName('button-null')[0].style.backgroundColor = 'lightgray'; i=1};
+}
